2ca6a69d1eab6d4b45e5998376d9a568283f6629
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-11 22:33:05 +0200

    Better image size

--- generate.py
+++ generate.py
@@ -101,7 +101,7 @@ def generate_image_math(package):
     texts = map("$\\{0}$".format, names)
     paths = [os.path.join("build", "math", package, "{0}.png".format(n))
              for n in names]
-    latex_to_pngs(texts, paths, (15, 15))
+    latex_to_pngs(texts, paths, (20, 20))
 
 
 def main(args=None):
